046b47ed28244a3bfdeac24739c356bc70cbb671
BrBrov <epoc@list.ru>
2024-03-02 12:28:40 +0300

    docs: delete old files

--- portfolio/src/portfolio.tsx
+++ portfolio/src/portfolio.tsx
@@ -1,15 +1,12 @@
 import { Analytics } from '@vercel/analytics/react';
 import './portfolio.scss';
 import './modules/background/bground.tsx';
-import Bground from './modules/background/bground.tsx';
-import CV from './modules/cv/cv.tsx';
 
 function Portfolio() {
   return (
     <>
       <Analytics debug = { false }/>
-      <Bground />
-      <CV />
+      <span>Portfolio</span>
     </>
   );
 }
