3a930dbbb6947ac7c3e10d2571d9fc386db8e39e
BrBrov <epoc@list.ru>
2024-03-23 17:00:17 +0300

    feat: create adaptive to 670px

--- portfolio/src/components/contacts/contacts-list/contacts-list.tsx
+++ portfolio/src/components/contacts/contacts-list/contacts-list.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from 'react';
-import { ContactsListProps } from '../../../types/contactsList-type';
+import { ContactsListProps } from '../../../types/contact-list-type';
 import { ContactsRecord } from '../../../types/contacts-types';
 import ContactRecord from './contact-record/contact-record';
+import './contacts-list.scss';
 
 function ContactsList({contacts}: ContactsListProps): ReactElement {
   return (
-    <div className='contacts-list'>
+    <div className='contacts__list'>
       {contacts.map((record: ContactsRecord, index: number) => (
         <ContactRecord key={index + record.title.en} record={record}/>
       ))}
