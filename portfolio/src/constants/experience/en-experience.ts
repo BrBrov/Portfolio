29da201007e89672a4bf8773a5cdd312ed044aea
BrBrov <epoc@list.ru>
2024-03-29 17:53:46 +0300

    fix: fix experience desription

--- portfolio/src/constants/experience/en-experience.ts
+++ portfolio/src/constants/experience/en-experience.ts
@@ -4,36 +4,34 @@ const ReactExperience: Experience = {
   professional: 'React 2023 Q4',
   period: '2023-2024',
   organization: 'RS School',
-  description: 'I performed localization into different ' + 
-               'languages using context, configured the ' + 
-               'state using the Redux Toolkit, and wrote ' + 
-               'the logic for receiving and sending GraphQL ' + 
-               'queries using regular POST queries via RTK Query. ' + 
-               'And he created many components on React. I used Next ' + 
-               'JS in the project.'
+  description: 'Localized the application into different ' +
+               'languages ​​using context, configured the state ' +
+               'using Redux Toolkit and wrote the logic for ' +
+               'receiving and sending GraphQL requests using regular ' +
+               'POST requests via RTK Query. Created many application ' +
+               'components using React. I used Vite and Next JS in the ' +
+               'projects.'
 };
 
 const AngularExperience: Experience = {
   professional: 'Angular 2023Q1',
   period: '2022-2023',
   organization: 'RS School',
-  description: 'Used in the educational project NgRx, RxJS, ' + 
-               'Material Design. I created application components, ' + 
-               'added services for interactivity of components, ' + 
-               'changed the state of NgRx using Action, added logic ' + 
-               'to the Reducer to display State.'
+  description: 'Used NgRx, RxJS, Material Design in an educational' +
+               ' project. I created application components, added ' +
+               'services for component interactivity, added NgRx logic, ' +
+               'used Effect to send requests to the server.'
 };
 
 const JsExperience: Experience = {
   professional: 'JavaScript/Front-end 2022Q3',
   period: '2022-2023',
   organization: 'RS School',
-  description: 'Adaptive HTML/CSS layout based on figma layouts, ' + 
-               'PSD templates. Writing elements on a page using JS ' + 
-               'to receive data from third-party services (Flickr, ' + 
-               'OpenWeatherMap API), creating interactive elements ' + 
-               '(adaptive carousel, music player, animation using ' + 
-               'the Web Animation API).'
+  description: 'Adaptive HTML/CSS layout based on Figma layouts and ' +
+               'PSD templates. Creating elements on a page using JS/TS, ' +
+               'receiving data from different services (Flickr, ' + 
+               'OpenWeatherMap API), creating adaptive elements (carousel, ' +
+               'music player, animation using Web Animation API).'
 };
 
 const enExperience: Array<Experience> = [
--- portfolio/src/constants/experience/ru-experience.ts
+++ portfolio/src/constants/experience/ru-experience.ts
@@ -4,23 +4,23 @@ const ReactExperience: Experience = {
   professional: 'React 2023 Q4',
   period: '2023-2024',
   organization: 'RS School',
-  description: 'Я выполнил локализацию на разные языки с ' + 
+  description: 'Выполнил локализацию приложения на разные языки с ' + 
                'использованием контекста, настроил состояние с ' + 
                'помощью Redux Toolkit и написал логику получения и ' +
                'отправки запросов GraphQL с помощью обычных запросов ' + 
-               'POST через RTK Query. И он создал множество компонентов ' + 
-               'на React. В проекте я использовал Next JS.'
+               'POST через RTK Query. Создал множество компонентов прилжения' + 
+               ' на React. В проектах я использовал Vite и Next JS.'
 };
 
 const AngularExperience: Experience = {
   professional: 'Angular 2023Q1',
   period: '2022-2023',
   organization: 'RS School',
-  description: 'Используется в образовательном проекте NgRx, RxJS, ' + 
+  description: 'Использовал в образовательном проекте NgRx, RxJS, ' + 
                'Material Design. Я создал компоненты приложения, ' + 
                'добавил сервисы для интерактивности компонентов, ' + 
-               'изменил состояние NgRx с помощью Action, добавил ' + 
-               'в Редюсер логику для отображения State.'
+               'добавил логику NgRx, использовал Effect для отправки' + 
+               'запросов на сервер.'
 };
 
 const JsExperience: Experience = {
@@ -28,10 +28,10 @@ const JsExperience: Experience = {
   period: '2022-2023',
   organization: 'RS School',
   description: 'Адаптивная HTML/CSS-верстка на основе макетов ' +
-               'Figma, PSD-шаблонов. Написание элементов на странице ' +
-               'с помощью JS для получения данных от сторонних сервисов ' +
-               '(Flickr, OpenWeatherMap API), создание интерактивных ' +
-               'элементов (адаптивная карусель, музыкальный проигрыватель, ' + 
+               'Figma, PSD-шаблонов. Создание элементов на странице ' +
+               'с помощью JS/TS, получение данных от разных сервисов ' +
+               '(Flickr, OpenWeatherMap API), создание адаптивных ' +
+               'элементов (карусель, музыкальный проигрыватель, ' + 
                'анимация с использованием Web Animation API).'
 };
 
